e03db14fcba7b650b7cc01761192f61b5eec6143
Yang Liu <liuyangc3@gmail.com>
2020-03-13 17:46:57 +0800

    Update docker_wrapper.py

--- docker_wrapper.py
+++ docker_wrapper.py
@@ -9,20 +9,18 @@ import sys
 #  k8s.gcr.io/xxx:yyy => gcr.io/google-containers/xxx:yyy -> gcr.azk8s.cn/google-containers/xxx:yyy, for example k8s.gcr.io/kube-apiserver:v1.14.1
 #  quay.io/xxx/yyy:zzz -> quay.azk8s.cn/xxx/yyy:zzz, for example quay.io/coreos/flannel:v0.10.0-amd64
 
-converts = [
-    {
-        'prefix': 'gcr.io',
-        'replace': lambda x: x.replace('gcr.io', 'gcr.azk8s.cn'),
-    },
-    {
-        'prefix': 'k8s.gcr.io',
-        'replace': lambda x: x.replace('k8s.gcr.io', 'gcr.azk8s.cn/google-containers'),
-    },
-    {
-        'prefix': 'quay.io',
-        'replace': lambda x: x.replace('quay.io', 'quay.azk8s.cn'),
-    }
-]
+proxys = {
+    'gcr.io': 'gcr.azk8s.cn',
+    'k8s.gcr.io': 'gcr.azk8s.cn/google-containers',
+    'quay.io': 'quay.azk8s.cn',
+}
+
+def proxy_image(image):
+    repo = image.split("/")[0]
+    if repo in proxys:
+        return  image.replace(repo, proxys[repo])
+    # docker hub
+    return  'dockerhub.azk8s.cn/library/' + image
 
 def execute_sys_cmd(cmd):
     result = os.system(cmd)
@@ -39,24 +37,18 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     image = sys.argv[2]
-    imageArray = image.split("/")
-   
-    newImage = '' 
-    for cvt in converts:
-        if imageArray[0] == cvt['prefix']:
-            newImage = cvt['replace'](image)
-            break
+    newImage = proxy_image(image)
     if newImage:
         print("-- pull {image} from {newimage} instead --".format(image=image, newimage=newImage))
         cmd = "docker pull {image}".format(image=newImage)
         execute_sys_cmd(cmd)
-     
+
         cmd = "docker tag {newImage} {image}".format(newImage=newImage, image=image)
         execute_sys_cmd(cmd)
-       
+
         cmd = "docker rmi {newImage}".format(newImage=newImage)
         execute_sys_cmd(cmd)
-       
+
         print("-- pull {image} done --".format(image=image))
         sys.exit(0)
     else:
